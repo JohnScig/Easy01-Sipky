d5bf22607a6f24a78d051b639bad3551a9d9261c
John Scigulinsky <john.scigulinsky@gmail.com>
2019-03-21 12:35:55 +0100

    Program works

--- Transformer09c/Transformer09c/Program.cs
+++ Transformer09c/Transformer09c/Program.cs
@@ -10,7 +10,47 @@ namespace Transformer09c
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            while (true)
+            {
+                Console.WriteLine($"You've hit for {Dart()} points.");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        static int Dart()
+        {
+            Console.WriteLine("Please input number for x-axis");
+            float x = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please input number for x-axis");
+            float y = float.Parse(Console.ReadLine());
+
+            int points = EvaluateDartThrow(x, y);
+
+
+            return points;
+        }
+
+        static int EvaluateDartThrow(float x, float y)
+        {
+            float distance = (float)Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+            if (distance > 10)
+            {
+                return 0;
+            }
+            else if (distance > 5)
+            {
+                return 1;
+            }
+            else if (distance > 1)
+            {
+                return 5;
+            }
+            else if (distance <= 1)
+            {
+                return 10;
+            }
+            return 0;
         }
     }
 }
